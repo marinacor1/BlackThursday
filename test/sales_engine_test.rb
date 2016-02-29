e7ff54907fb185d2847bb19c7fd29b2571682ed9
marinacor1 <marinaspam1234@gmail.com>
2016-02-29 15:25:15 -0700

    initial tests

---
+++ test/sales_engine_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/sales_engine'
+
+class SalesEngineTest < Minitest::Test
+
+  def test_sales_engine_initializes
+
+  end
+
+  def test_
+
+end
