1119e9ca5f9aa0727cd56c689d0d779a7f32308b
Kami Boers <kamiboers@gmail.com>
2016-02-29 18:19:51 -0700

    removed redundancy from sales engine.

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -7,14 +7,8 @@ attr_reader :items, :merchants
 def initialize
   @items = ItemRepository.new
   @merchants = MerchantRepository.new
-  load_items
-  load_merchants
+
 end
 
-def load_items
-end
-
-def load_merchants
-end
 
 end
