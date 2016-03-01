e23e5d16899b7334c690762033b3bde38296daf1
marinacor1 <marinaspam1234@gmail.com>
2016-03-01 09:47:35 -0700

    adding sales analyst

---
+++ test/sales_analyst_test.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/sales_analyst'
+
+class SalesAnalystTest < Minitest::Test
+
+  def test_sales_engine_instantiates_from_csv
+
+  end
+end
