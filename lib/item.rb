bcbdf97c3723a8db62288b6cc680e425afb5fc6f
Kami Boers <kamiboers@gmail.com>
2016-02-29 21:13:54 -0700

    trying out different ways for classes to interrelate.

---
+++ lib/item.rb
@@ -0,0 +1,11 @@
+class Item
+attr_accessor :id, :name, :description, :unit_price, :merchant_id, :created_at, :updated_at, :unit_price_to_dollars
+  def initialize
+  end
+
+  # def unit_price_to_dollars
+  #   @unit_price = self.unit_price/100.0
+  # end
+
+
+end
--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -3,12 +3,32 @@ require_relative 'item_repository'
 require_relative 'merchant_repository'
 
 class SalesEngine
-attr_reader :items, :merchants
+attr_accessor :items, :merchants
 def initialize
-  @items = ItemRepository.new
-  @merchants = MerchantRepository.new
+
+end
+
+def self.from_csv(hash)
+
+  items = ItemRepository.new
+  items.populate_items_with_data_from_csv(hash[:items])
+  @items = items
+binding.pry
+  # @merchants = MerchantRepository.new
+  # @merchants.populate_items_with_data_from_csv(hash[:merchants])
+
+  self
 
 end
 
 
+end
+
+if __FILE__ == $0
+se = SalesEngine.new
+se = SalesEngine.from_csv({
+  :items     => "../data/items.csv",
+  :merchants => "../data/merchants.csv"
+})
+binding.pry
 end
