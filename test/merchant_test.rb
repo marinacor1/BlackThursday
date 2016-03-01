0cabe2d6e767e955afe33e2ceb4870eea071740c
marinacor1 <marinaspam1234@gmail.com>
2016-03-01 12:18:14 -0700

    merchant initial tests passing

--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -5,7 +5,13 @@ require_relative '../lib/merchant'
 class MerchantTest < Minitest::Test
   def test_merchant_instantiates
     m = Merchant.new({:id => 5, :name => "Buffalo Exchange"})
-    m.instance_of? Merchant 
+    m.instance_of? Merchant
+  end
+
+  def test_merchant_access_id_and_name_from_data
+    m = Merchant.new({:id => 5, :name => "Buffalo Exchange"})
+    assert_equal 5, m.id
+    assert_equal "Buffalo Exchange", m.name
   end
 
 end
