b1f3cb20f2b3034ac6b428aaa08db3d437e2d1ec
marinacor1 <marinaspam1234@gmail.com>
2016-03-02 11:48:21 -0700

    adding old test from merchant test

--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -5,16 +5,23 @@ require_relative '../lib/sales_engine'
 
 class MerchantTest < Minitest::Test
   def test_merchant_instantiates
-    m = Merchant.new({:id => 5, :name => "Turing School"})
-    m.instance_of? Merchant
+    m = Merchant.new({:id => 5, :name => 'Payless'})
+    assert m.instance_of? Merchant
   end
 
-  def test_merchant_access_id_and_name_from_data
+  def test_merchant_access_name_from_data
     hash = {:items => "./data/items.csv", :merchants => "./data/subsets/merchants_small.csv"}
     se = SalesEngine.from_csv(hash)
     mr = se.merchants
     assert_equal mr.all[1].name, "Candisart"
     assert mr.all[0].instance_of? Merchant
+  end
+
+  def test_merchant_access_id_from_data
+    hash = {:items => "./data/items.csv", :merchants => "./data/subsets/merchants_small.csv"}
+    se = SalesEngine.from_csv(hash)
+    mr = se.merchants
+    assert_equal 12334105, mr.all[0].id
   end
 
 
