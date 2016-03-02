cdb881cbef4a2c12ccd646e447bfcce968eadefa
marinacor1 <marinaspam1234@gmail.com>
2016-03-02 10:57:13 -0700

    merchant repo is from work last night

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -5,20 +5,39 @@ require_relative 'merchant'
 require_relative 'item_repository'
 
 class MerchantRepository
-  attr_reader :all
+  attr_accessor :all_merchants, :name
 
   def initialize(path)
     @all_merchants = []
-    populate_merchant_repo_with_data_from_csv(path)
+    if path.include?'.csv'
+      populate_merchant_repo_with_data_from_csv(path)
+    else
+      populate_merchant_repo_with_hash(path)
+    end
   end
 
   def populate_merchant_repo_with_data_from_csv(path)
-    CSV.foreach(path, { headers: true, header_converters: :symbol, converters: :all}) do |data_row|
-      merchant = Merchant.new(data_row)
+    merchant_contents = CSV.open path, headers: true, header_converters: :symbol
+    loaded_merchants = merchant_contents.to_a.map {|row| row.to_h}
+    CSV.foreach(path, { headers: true, header_converters: :symbol, converters: :all}) do |row|
+      merchant = Merchant.new(loaded_merchants)
+      merchant.id = row[:id]
+      merchant.name = row[:name]
+      merchant.created_at = row[:created_at]
+      merchant.updated_at = row[:updated_at]
       # merchant.items = []
       # merchant.items << @all_items.find_by_id(merchant.id)
       @all_merchants << merchant
     end
+  end
+
+  def populate_merchant_repo_with_hash(path)
+    merchant = Merchant.new(path)
+    merchant.id = path[:id]
+    merchant.name = path[:name]
+    merchant.created_at = path[:created_at]
+    merchant.updated_at = path[:updated_at]
+    @all_merchants << merchant
   end
 
   def all
@@ -48,6 +67,50 @@ class MerchantRepository
   end
 
 end
+
+
+# def load_data(path)
+#   merchant_contents = CSV.open path, headers: true, header_converters: :symbol
+#   @loaded_merchants = merchant_contents.to_a.map {|row| row.to_h}
+#   contents = CSV.open './data/merchants.csv', headers: true, header_converters: :symbol
+#   @all_names = []
+#   @all_ids = []
+#   contents.each do |row|
+#     @all_ids << @id = row[:id]
+#     @all_names << @name = row[:name]
+#     created_at = row[:created_at]
+#     updated_at = row[:updated_at]
+#   end
+# end
+
+# def self.from_csv(path)
+#   binding.pry #oesn't hit binding
+#   contents = CSV.open '../data/merchants.csv', headers: true, header_converters: :symbol
+#   contents.each do |row|
+#     binding.pry
+#     @id = row[:id]
+#     @name = row[:name]
+#     @created_at = row[:created_at]
+#     @updated_at = row[:updated_at]
+#     puts @name
+#   end
+# end
+#
+#
+# end
+#define initalization methods that takes
+#takes into account design
+#we are giving it this string
+#parsing could happen here
+#you could pass it in already parsed data (item objects)
+
+
+#Jan's suggestion:
+#I would have a from_csv on merchants repository classes
+#repository classes would handle their own data(parse the data)
+#parse and organize data
+
+#sales engine can do manipulation and business logic
 
 if __FILE__ == $0
   se = SalesEngine.from_csv({
