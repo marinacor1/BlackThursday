d6f744f3f2654bab164e20260d2384208edeb58d
marinacor1 <marinaspam1234@gmail.com>
2016-03-02 10:49:00 -0700

    adding tests to sales engine and item

--- test/item_test.rb
+++ test/item_test.rb
@@ -17,4 +17,21 @@ def test_it_instantiates_an_item_object
   assert item.instance_of? Item
 end
 
+def test_item_class_has_access_to_item_info
+  data = {
+    :name        => "Pencil",
+    :description => "You can use it to write things",
+    :unit_price  => BigDecimal.new(10.99,4),
+    :created_at  => Time.now,
+    :updated_at  => Time.now,
+  }
+  item = Item.new(data)
+  assert_equal "Pencil", item.name
+  assert_equal "You can use it to write things", item.description
+  # assert_in_delta BigDecimal.new(10.99,5), (500.0 / 10**6), item.unit_price
+  # assert_equal Time.now, item.created_at
+  # assert_equal Time.now, item.updated_at
+
+end
+
 end
