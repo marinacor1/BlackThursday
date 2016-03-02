28986c0b343266553b7879a2fad1de3cb2b5fcf0
marinacor1 <marinaspam1234@gmail.com>
2016-03-01 22:28:43 -0700

    adding test for id to merchant test

--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -9,12 +9,19 @@ class MerchantTest < Minitest::Test
     m.instance_of? Merchant
   end
 
-  def test_merchant_access_id_and_name_from_data
+  def test_merchant_access_name_from_data
     hash = {:items => "./data/items.csv", :merchants => "./data/subsets/merchants_small.csv"}
     se = SalesEngine.from_csv(hash)
     mr = se.merchants
     assert_equal mr.all[1].name, "Candisart"
     assert mr.all[0].instance_of? Merchant
+  end
+
+  def test_merchant_access_id_from_data
+    hash = {:items => "./data/items.csv", :merchants => "./data/subsets/merchants_small.csv"}
+    se = SalesEngine.from_csv(hash)
+    mr = se.merchants
+    assert_equal 12334105, mr.all[0].id
   end
 
 
