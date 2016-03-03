300e5dfae2ce25a9d3389c746d5915f31bc240cd
marinacor1 <marinaspam1234@gmail.com>
2016-03-03 14:19:14 -0700

    both name tests pass

--- test/repository_test.rb
+++ test/repository_test.rb
@@ -18,7 +18,7 @@ class RepositoryTest < Minitest::Test
     hash = {:items => "./data/items.csv", :merchants => "./data/merchants.csv"}
     se = SalesEngine.from_csv(hash)
     mr = se.merchants
-    merchant = mr.find_by_name("Marinas Shop")
+    merchant = mr.find_with_name(mr.all, "Marinas Shop")
     assert_equal nil, merchant
   end
 
