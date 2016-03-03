f46a7f292131dcb530da30adcb6d8c37c0279e8e
marinacor1 <marinaspam1234@gmail.com>
2016-03-02 19:54:59 -0700

    initial skeletong

---
+++ lib/invoice_repository.rb
@@ -0,0 +1,16 @@
+require 'pry'
+class InvoiceRepository
+
+  def all
+    #returns an array of all known invoice instances
+  end
+
+  def find_by_id(id_num)
+    #returs nil if no match
+    #or returns instances of invoice with matching id
+  end
+
+  def find
+
+
+end
