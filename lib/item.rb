ba275f360726b7d7595c146af1651f590d77bd11
Kami Boers <kamiboers@gmail.com>
2016-03-03 15:16:06 -0700

    progress on 2

--- lib/item.rb
+++ lib/item.rb
@@ -4,21 +4,12 @@ attr_accessor :id, :name, :description, :unit_price, :merchant_id, :created_at, 
     @id = attributes[:id]
     @name = attributes[:name]
     @description = attributes[:description]
-    @unit_price = attributes[:unit_price]
+    @unit_price = attributes[:unit_price].to_f
     @merchant_id = attributes[:merchant_id]
-    @created_at = attributes[:created_at]
-    @updated_at = attributes[:updated_at]
+    @created_at = Date.parse(attributes[:created_at].to_s)
+    @updated_at = Date.parse(attributes[:updated_at].to_s)
     unit_price_to_dollars
   end
-
-  # def unit_price
-  #   num = @unit_price.to_f.to_s.length
-  #   BigDecimal.new(@unit_price.to_f,num)
-  # end
-  #
-  # def created_at
-  #   Time.parse(@created_at)
-  # end
 
   def unit_price_to_dollars
     @unit_price = self.unit_price/100.0
@@ -26,16 +17,16 @@ attr_accessor :id, :name, :description, :unit_price, :merchant_id, :created_at, 
 
 end
 
-#
-# if __FILE__ == $0
-#
-#   data = {
-#     :name        => "Pencil",
-#     :description => "You can use it to write things",
-#     :unit_price  => BigDecimal.new(10.99,4),
-#     :created_at  => Time.now,
-#     :updated_at  => Time.now,
-#   }
-#   item = Item.new(data)
-#
-# end
+
+if __FILE__ == $0
+
+  data = {
+    :name        => "Pencil",
+    :description => "You can use it to write things",
+    :unit_price  => BigDecimal.new(10.99, 4),
+    :created_at  => Time.now,
+    :updated_at  => Time.now,
+  }
+  item = Item.new(data)
+
+end
