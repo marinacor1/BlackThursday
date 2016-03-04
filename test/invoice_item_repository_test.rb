ae25cc3babffe335eb648edf05f485e12cbca727
marinacor1 <marinaspam1234@gmail.com>
2016-03-03 20:07:59 -0700

    adding tests for all an find by id

--- test/invoice_item_repository_test.rb
+++ test/invoice_item_repository_test.rb
@@ -1,10 +1,24 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require_relative '../lib/invoice_repository'
-require_relative '../lib/sales_engine'
+require_relative '../lib/invoice_item_repository'
 require 'pry'
 
-class InvoiceRepositoryTest < Minitest::Test
+class InvoiceItemRepositoryTest < Minitest::Test
+  def test_invoice_item_repository_instantiates
+    skip
+    ir = InvoiceItemRepository.new
+    ir.from_csv("./data/invoice_items.csv")
+    assert ir.instance_of? InvoiceItemRepository
+  end
+
+  def test_invoice_item_repo_returns_all_instances
+    skip
+    ir = InvoiceItemRepository.new
+    ir.from_csv("./data/subsets/invoice_items_small.csv")
+    invoices = ir.all
+    assert_equal 10, invoices.count
+    assert_equal Invoice, invoices[0].class
+  end
 
   def test_invoice_repo_can_find_invoice_with_item_id
     skip
@@ -15,12 +29,11 @@ class InvoiceRepositoryTest < Minitest::Test
     assert_equal Invoice, invoice[0].class
   end
 
-  def test_invoice_repo_can_find_all_invoices_with_item_id
+  def test_invoice_repo_returns_nil_if_find_by_id_has_wrong_id
     skip
     ir = InvoiceItemRepository.new
     ir.from_csv("./data/invoice_items.csv")
-    invoice = ir.find_by_id(6)
-    assert_equal 'invoice', invoice
-    assert_equal Invoice, invoice[0].class
+    invoice = ir.find_by_id(6745)
+    assert_equal nil, invoice
   end
-end 
+end
