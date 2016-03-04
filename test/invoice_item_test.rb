3927431b76c493467e21e4012045e2009dd499d8
marinacor1 <marinaspam1234@gmail.com>
2016-03-03 20:59:23 -0700

    second attributes test

--- test/invoice_item_test.rb
+++ test/invoice_item_test.rb
@@ -15,7 +15,27 @@ class InvoiceItemTest < Minitest::Test
   :created_at => Time.now,
   :updated_at => Time.now
 })
-   assert ii.instance_of? InvoiceItem 
+   assert ii.instance_of? InvoiceItem
+  end
+
+  def test_invoice_items_knows_its_attributes
+    skip
+    ii = InvoiceItem.new({
+  :id => 6,
+  :item_id => 7,
+  :invoice_id => 8,
+  :quantity => 1,
+  :unit_price => BigDecimal.new(10.99, 4),
+  :created_at => Time.now,
+  :updated_at => Time.now
+})
+   assert_equal 6, ii.id
+   assert_equal 7, ii.item_id
+   assert_equal 8, ii.invoice_id
+   assert_equal 1, ii.quantity
+   assert_equal 10.99, ii.unit_price.to_f
+   assert_equal Time.now, ii.created_at
+   assert_equal Time.now, ii.updated_at 
   end
 
 end
