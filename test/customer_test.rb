a23d336d39d687931c475b48fa46979e7b17c543
marinacor1 <marinaspam1234@gmail.com>
2016-03-03 22:54:18 -0700

    finished tests for ct

--- test/customer_test.rb
+++ test/customer_test.rb
@@ -6,32 +6,30 @@ require 'pry'
 class CustomerTest < Minitest::Test
   def test_customer_instantiates
     skip
-    t = Customer.new({
+    c = Customer.new({
   :id => 6,
   :first_name => "Joan",
   :last_name => "Clarke",
   :created_at => Time.now,
   :updated_at => Time.now
 })
-    t.instance_of? Customer
+    c.instance_of? Customer
   end
 
   def test_customer_knows_its_attributes
     skip
-    t = Customer.new({
+    c = Customer.new({
   :id => 6,
   :first_name => "Joan",
   :last_name => "Clarke",
   :created_at => Time.now,
   :updated_at => Time.now
 })
-    assert_equal 6, t.id
-    assert_equal 8, t.invoice_id
-    assert_equal "4242424242424242", t.credit_card_number
-    assert_equal "0220", t.credit_card_expiration_date
-    assert_equal "success", t.result
-    assert_equal Time.now, t.created_at
-    assert_equal Time.now, t.updated_at
+    assert_equal 6, c.id
+    assert_equal "Joan", c.first_name
+    assert_equal "Clarke", c.last_name
+    assert_equal Time.now, c.created_at
+    assert_equal Time.now, c.updated_at
   end
 
 
