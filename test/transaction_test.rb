2cf450f811a20049813fec2e53717d2acb6fd7a2
marinacor1 <marinaspam1234@gmail.com>
2016-03-03 22:26:11 -0700

    finish test

--- test/transaction_test.rb
+++ test/transaction_test.rb
@@ -31,10 +31,10 @@ class TransactionTest < Minitest::Test
     assert_equal 6, t.id
     assert_equal 8, t.invoice_id
     assert_equal "4242424242424242", t.credit_card_number
-    assert_equal "0220", t.credit_card_expiration_date 
-    assert_equal 6, t.id
-    assert_equal 6, t.id
-    assert_equal 6, t.id
+    assert_equal "0220", t.credit_card_expiration_date
+    assert_equal "success", t.result
+    assert_equal Time.now, t.created_at
+    assert_equal Time.now, t.updated_at
   end
 
 
