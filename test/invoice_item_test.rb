8302f96465c23cc100d5b0fc986cf560ab272437
marinacor1 <marinaspam1234@gmail.com>
2016-03-03 20:54:55 -0700

    adding initial structure

--- test/invoice_item_test.rb
+++ test/invoice_item_test.rb
@@ -4,5 +4,18 @@ require_relative '../lib/invoice_item'
 require 'pry'
 
 class InvoiceItemTest < Minitest::Test
+  def test_invoice_items_instantiates
+    skip
+    ii = InvoiceItem.new({
+  :id => 6,
+  :item_id => 7,
+  :invoice_id => 8,
+  :quantity => 1,
+  :unit_price => BigDecimal.new(10.99, 4),
+  :created_at => Time.now,
+  :updated_at => Time.now
+})
+   assert ii.instance_of? InvoiceItem 
+  end
 
 end
