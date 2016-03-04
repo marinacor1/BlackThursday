5e206b789c65be3f9ac28e757e39666139b726ba
marinacor1 <marinaspam1234@gmail.com>
2016-03-04 16:42:24 -0700

    adding iter 3 test to se

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -30,4 +30,70 @@ class SalesEngineTest < Minitest::Test
     assert_equal 1367, se.items.count
   end
 
+  def test_sales_engine_can_receive_information_from_many_csvs
+    skip
+    se = SalesEngine.from_csv({
+  :items => "./data/items.csv",
+  :merchants => "./data/merchants.csv",
+  :invoices => "./data/invoices.csv",
+  :invoice_items => "./data/invoice_items.csv",
+  :transactions => "./data/transactions.csv",
+  :customers => "./data/customers.csv"
+})
+   assert items.instance_of? ItemRepository
+   assert merchants.instance_of? MerchantRepository
+   assert invoices.instance_of? InvoiceRepository
+   assert invoice_items.instance_of? InvoiceItemRepository
+   assert transactions.instance_of? TransactionRepository
+   assert customers.instance_of? CustomerRepository
+  end
+
+  def test_sales_engine_can_find_connections_from_an_invoice
+    skip
+    se = SalesEngine.from_csv({
+  :items => "./data/items.csv",
+  :merchants => "./data/merchants.csv",
+  :invoices => "./data/invoices.csv",
+  :invoice_items => "./data/invoice_items.csv",
+  :transactions => "./data/transactions.csv",
+  :customers => "./data/customers.csv"
+})
+   invoice = se.invoices.find_by_id(20)
+   invoice_items ['i', 'i']
+   assert_equal invoice_items, invoice.items
+   transactions = ['t', 't']
+   assert_equal transactions, invoice.transactions
+   customer = 'person'
+   assert_equal customer, invoice.customer
+  end
+
+  def test_sales_engine_can_find_connections_from_an_invoice
+    skip
+    se = SalesEngine.from_csv({
+  :items => "./data/items.csv",
+  :merchants => "./data/merchants.csv",
+  :invoices => "./data/invoices.csv",
+  :invoice_items => "./data/invoice_items.csv",
+  :transactions => "./data/transactions.csv",
+  :customers => "./data/customers.csv"
+  })
+   transaction = se.invoices.find_by_id(40)
+   invoice = 'invoice'
+   assert_equal invoice, transaction.invoice
+  end
+
+  def test_sales_engine_can_find_mechants_from_customer_side 
+    skip
+    se = SalesEngine.from_csv({
+  :items => "./data/items.csv",
+  :merchants => "./data/merchants.csv",
+  :invoices => "./data/invoices.csv",
+  :invoice_items => "./data/invoice_items.csv",
+  :transactions => "./data/transactions.csv",
+  :customers => "./data/customers.csv"
+  })
+   merchant = se.invoices.find_by_id(10)
+   customer_array = ['c', 'c']
+   assert_equal cs, merchant.customers
+  end
 end
