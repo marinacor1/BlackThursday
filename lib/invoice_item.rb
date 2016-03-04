ddfb6241aaad5fcde9bc96b7cdfdaf7111c090ad
marinacor1 <marinaspam1234@gmail.com>
2016-03-03 20:47:24 -0700

    invoice items added

---
+++ lib/invoice_item.rb
@@ -0,0 +1,4 @@
+class InvoiceItem
+
+
+end
---
+++ test/invoice_item_test.rb
@@ -0,0 +1,8 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/invoice_item'
+require 'pry'
+
+class InvoiceItemTest < Minitest::Test
+
+end
