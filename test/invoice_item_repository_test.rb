beac4550534be9da731acc383b6916439adde54c
marinacor1 <marinaspam1234@gmail.com>
2016-03-03 20:12:05 -0700

    ading test for find all iby item id

--- test/invoice_item_repository_test.rb
+++ test/invoice_item_repository_test.rb
@@ -17,23 +17,32 @@ class InvoiceItemRepositoryTest < Minitest::Test
     ir.from_csv("./data/subsets/invoice_items_small.csv")
     invoices = ir.all
     assert_equal 10, invoices.count
-    assert_equal Invoice, invoices[0].class
+    assert_equal InvoiceItem, invoices[0].class
   end
 
   def test_invoice_repo_can_find_invoice_with_item_id
     skip
     ir = InvoiceItemRepository.new
     ir.from_csv("./data/invoice_items.csv")
-    invoice = ir.find_by_id(6)
-    assert_equal 'invoice', invoice
-    assert_equal Invoice, invoice[0].class
+    invoice_item = ir.find_by_id(6)
+    assert_equal 'invoice', invoice_item
+    assert_equal InvoiceItem, invoice_item[0].class
   end
 
   def test_invoice_repo_returns_nil_if_find_by_id_has_wrong_id
     skip
     ir = InvoiceItemRepository.new
     ir.from_csv("./data/invoice_items.csv")
-    invoice = ir.find_by_id(6745)
-    assert_equal nil, invoice
+    invoice_item = ir.find_by_id(6745)
+    assert_equal nil, invoice_item
+  end
+
+  def test_invoice_repo_gives_all_matching_items_by_find_all_by_item_id
+    skip
+    ir = InvoiceItemRepository.new
+    ir.from_csv("./data/invoice_items.csv")
+    invoice_items = ir.find_all_by_item_id(7)
+    assert_equal [], invoice_items
+    assert_equal InvoiceItem, invoice_items.class
   end
 end
