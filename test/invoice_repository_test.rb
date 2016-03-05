f6f3f20efde0c4a9efe86f65e9963b0d2af74af9
marinacor1 <marinaspam1234@gmail.com>
2016-03-05 11:08:55 -0700

    find by id works

--- test/invoice_repository_test.rb
+++ test/invoice_repository_test.rb
@@ -26,12 +26,11 @@ class InvoiceRepositoryTest < Minitest::Test
   end
 
   def test_find_by_id_returns_correct_invoice
-    skip
     se = SalesEngine.from_csv({:invoices => './data/invoices.csv'})
     ir = se.invoices
     correct_invoice = ir.find_by_id(3451)
-    assert_equal 679, correct_invoice.merchant_id
-    assert_equal 12335337, correct_invoice.customer_id
+    assert_equal 12335337, correct_invoice.merchant_id
+    assert_equal 679, correct_invoice.customer_id
     assert_equal 'returned', correct_invoice.status
     assert_equal Invoice, correct_invoice.class
   end
