032c1a3598375853a2c4f016ba37b9365e00390f
marinacor1 <marinaspam1234@gmail.com>
2016-03-05 21:29:41 -0700

    adding test for avg avg using subset

--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -51,18 +51,11 @@ class SalesAnalystTest < Minitest::Test
     se = SalesEngine.from_csv(hash)
     sa = SalesAnalyst.new(se)
     sa.begin_analysis
+
     answer = 556.04
     calculated = sa.average_items_per_merchant_standard_deviation
     assert_equal answer, calculated
     assert_equal Float, calculated.class
-  end
-  def test_sales_analyst_returns_avg_item_std_deviation_from_subset
-    hash = {:items => "./data/items.csv", :merchants => "./data/merchants.csv", :invoices => "./data/invoices.csv"}
-    se = SalesEngine.from_csv(hash)
-    sa = SalesAnalyst.new(se)
-    sa.begin_analysis
-
-    assert_equal 3.26, sa.average_items_per_merchant_standard_deviation
   end
 
   def test_sales_analyst_returns_merchants_with_high_item_counts
@@ -104,6 +97,19 @@ class SalesAnalystTest < Minitest::Test
     assert_equal BigDecimal(answer, 5), avg_avg
     assert avg_avg.instance_of? BigDecimal
   end
+
+  def test_sa_can_find_average_average_price_across_all_merchants_with_subsets
+    hash = {:items => "./data/items.csv", :merchants => "./data/subsets/merchants_small.csv"}
+    se = SalesEngine.from_csv(hash)
+    sa = SalesAnalyst.new(se)
+    sa.begin_analysis
+    answer = 60.553
+    avg_avg = sa.average_average_price_per_merchant
+
+    assert_equal BigDecimal(answer, 5), avg_avg
+    assert avg_avg.instance_of? BigDecimal
+  end
+
 
   def test_sa_finds_all_golden_items
     hash = {:items => "./data/items.csv", :merchants => "./data/merchants.csv", :invoices => "./data/invoices.csv"}
