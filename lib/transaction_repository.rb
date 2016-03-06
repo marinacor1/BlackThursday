7e9f1fad17c4707c0ebb7c2bcce4c9c642c03fc6
marinacor1 <marinaspam1234@gmail.com>
2016-03-05 18:59:37 -0700

    adding transaction find by id and invoice id methods

--- lib/transaction_repository.rb
+++ lib/transaction_repository.rb
@@ -1,18 +1,54 @@
+require_relative 'transaction'
+require 'pry'
+require 'csv'
 class TransactionRepository
   def inspect
     true
   end
 
-  def all
-    #returns an array of all transaction instances
+  attr_accessor :all, :name
+
+  def initialize(path)
+    @all_transactions = []
+    populate_transaction_repo(path)
   end
 
-  def find_by_id(id)
-    #returns nil or transaction with a matching id
+  def populate_transaction_repo(path)
+    if path.include? '.csv'
+    CSV.foreach(path, { headers: true, header_converters: :symbol, converters: :all}) do |data_row|
+      transaction = Transaction.new(data_row)
+      @all_transactions << transaction
+    end
+    else
+      populate_transaction_repo_with_hash(path)
+    end
+  end
+
+  def populate_transaction_repo_with_hash(path)
+      path.each do
+      transaction = Transaction.new(path)
+      @all_transactions << transaction
+    end
+  end
+
+  def all
+    @all_transactions
+  end
+
+  def count
+    @all_transactions.count
+  end
+
+  def find_by_id(id_query)
+    @all_transactions.find do |element|
+      id_query == element.id
+    end
   end
 
   def find_all_by_invoice_id(id)
-    #returns [] or matches which have a matching credit card number
+    @all_transactions.find_all do |invoice|
+      invoice.invoice_id == id
+    end
   end
 
   def find_all_by_result(status)
