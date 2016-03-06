74f94e0b7ac2f03b3763f724bed920706d066058
marinacor1 <marinaspam1234@gmail.com>
2016-03-06 11:20:01 -0700

    fixing ir tests

--- test/invoice_repository_test.rb
+++ test/invoice_repository_test.rb
@@ -1,82 +1,72 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require_relative '../lib/invoice'
 require_relative '../lib/invoice_repository'
 require_relative '../lib/sales_engine'
 require 'pry'
 
 class InvoiceRepositoryTest < Minitest::Test
+  attr_reader :invoice1, :invoice2, :invoice3, :invoice4, :ir, :invoices
 
-  def test_invoice_repository_instantiates
-    se = SalesEngine.from_csv({:invoices => './data/invoices.csv'})
-    se.invoices.instance_of? InvoiceRepository
+  def setup
+    invoice1 = Invoice.new({id: 20, customer_id: 10, merchant_id: 100, status: 'shipped', created_at: 10-20-1995, updated_at: 12-18-2013 })
+    invoice2 = Invoice.new({id: 21, customer_id: 11, merchant_id: 101, status: 'shipped', created_at: 11-20-1995, updated_at: 02-30-1009 })
+    invoice3 = Invoice.new({id: 22, customer_id: 11, merchant_id: 100, status: 'pending', created_at: 10-20-1985, updated_at: 01-13-2013 })
+    invoice4 = Invoice.new({id: 23, customer_id: 11, merchant_id: 102, status: 'shipped', created_at: 12-21-1975, updated_at: 12-30-2015 })
+
+    @invoices = [invoice1, invoice2, invoice3, invoice4]
+
+    @ir = InvoiceRepository.new(@invoices)
   end
 
-  def test_invoice_repository_contains_invoice_instances
+  def test_invoice_repository_instantiates
+    ir.instance_of? InvoiceRepository
+  end
+
+  def test_invoice_repository_contains_invoice_instances_with_sales_engine
     se = SalesEngine.from_csv({:invoices => './data/invoices.csv'})
     ir = se.invoices
     ir.instance_of? InvoiceRepository
   end
 
   def test_all_returns_array_of_all_known_invoice_instances
-    se = SalesEngine.from_csv({:invoices => './data/invoices.csv'})
-    ir = se.invoices
     assert_equal Array, ir.all.class
     assert_equal Invoice, ir.all[0].class
-    assert_equal 4985, ir.all.count
-  end
-
-  def test_all_returns_array_of_all_known_invoice_instances_with_subset
-    se = SalesEngine.from_csv({:invoices => './data/subsets/invoice_small.csv'})
-    ir = se.invoices
-    assert_equal Array, ir.all.class
-    assert_equal Invoice, ir.all[0].class
-    assert_equal 9, ir.all.count
+    assert_equal 4, ir.all.count
   end
 
   def test_find_by_id_returns_correct_invoice
-    se = SalesEngine.from_csv({:invoices => './data/invoices.csv'})
-    ir = se.invoices
-    correct_invoice = ir.find_by_id(3451)
-    assert_equal 12335337, correct_invoice.merchant_id
-    assert_equal 679, correct_invoice.customer_id
-    assert_equal :returned, correct_invoice.status
+    correct_invoice = ir.find_by_id(20)
+    assert_equal 100, correct_invoice.merchant_id
+    assert_equal 10, correct_invoice.customer_id
+    assert_equal :shipped, correct_invoice.status
     assert_equal Invoice, correct_invoice.class
   end
 
   def test_find_by_id_returns_nil_for_wrong_id
-    se = SalesEngine.from_csv({:invoices => './data/invoices.csv'})
-    ir = se.invoices
     assert_equal nil, ir.find_by_id(1277777776)
   end
 
   def test_find_all_by_customer_id_returns_invoice_array
-    se = SalesEngine.from_csv({:invoices => './data/invoices.csv'})
-    ir = se.invoices
-    invoice_array = ir.find_all_by_customer_id(1)
+    invoice_array = ir.find_all_by_customer_id(11)
     assert_equal 8, invoice_array.count
     assert_equal Array, invoice_array.class
     assert_equal Invoice, invoice_array[0].class
   end
 
   def test_find_all_by_customer_id_returns_empty_array_for_wrong_id
-    se = SalesEngine.from_csv({:invoices => './data/invoices.csv'})
-    ir = se.invoices
     invoice_array =[]
-    assert_equal invoice_array, ir.find_all_by_customer_id(1797294798)
+    assert_equal invoice_array, ir.find_all_by_customer_id(10)
     assert_equal Array, invoice_array.class
   end
 
   def test_all_by_status_returns_all_matching_status
-    se = SalesEngine.from_csv({:invoices => './data/invoices.csv'})
-    ir = se.invoices
     status_array = ir.find_all_by_status('pending')
     assert_equal 1473, status_array.count
     assert_equal Array, status_array.class
   end
 
   def test_find_all_by_status_returns_empty_array_for_wrong_status
-    se = SalesEngine.from_csv({:invoices => './data/invoices.csv'})
-    ir = se.invoices
     status_array = []
     assert_equal status_array, ir.find_all_by_status('popcorn')
     assert_equal Array, status_array.class
