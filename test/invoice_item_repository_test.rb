1193e7ff17b6291f6e85f7268b51b4ce292c9829
marinacor1 <marinaspam1234@gmail.com>
2016-03-05 17:50:32 -0700

    fixing tests

--- test/invoice_item_repository_test.rb
+++ test/invoice_item_repository_test.rb
@@ -21,7 +21,6 @@ class InvoiceItemRepositoryTest < Minitest::Test
   end
 
   def test_invoice_repo_can_find_invoice_with_item_id
-    skip
     ir = InvoiceItemRepository.new
     ir.from_csv("./data/invoice_items.csv")
     invoice_item = ir.find_by_id(6)
