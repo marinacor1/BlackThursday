cd4263219728f79cdfc158db546b223dd9274e7c
marinacor1 <marinaspam1234@gmail.com>
2016-03-05 18:32:42 -0700

    wrong first name works

--- test/customer_repository_test.rb
+++ test/customer_repository_test.rb
@@ -45,11 +45,11 @@ class CustomerRepositoryTest < Minitest::Test
   end
 
   def test_customer_repo_returns_empty_array_with_wrong_first_name
-    skip
-    c = CustomerRepository.new
-    c.from_csv("./data/customers.csv")
-    customer = c.find_all_by_first_name('lollipops')
-    assert_equal [], customer
+    hash = {:items => "./data/items.csv", :merchants => "./data/merchants.csv", :invoices => './data/invoices.csv', :invoice_items => './data/invoice_items.csv', :customers => './data/customers.csv'}
+    c = SalesEngine.from_csv(hash)
+    customer = c.customers
+    answer = customer.find_all_by_first_name('lollipops')
+    assert_equal [], answer 
   end
 
   def test_it_returns_all_matching_last_name_customers
