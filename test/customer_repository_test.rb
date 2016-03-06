beff461a4975892cd2fab30a56fd9d50eb47cf70
marinacor1 <marinaspam1234@gmail.com>
2016-03-06 11:04:38 -0700

    cr works with hashes

--- test/customer_repository_test.rb
+++ test/customer_repository_test.rb
@@ -29,41 +29,35 @@ class CustomerRepositoryTest < Minitest::Test
   end
 
   def test_customer_repo_returns_customer_with_find_by_id
-    customer = cr.customers
-    answer = customer.find_by_id(1)
+    answer = cr.find_by_id(1)
     assert_equal 'Marina', answer.first_name
     assert_equal 'Corona', answer.last_name
     assert_equal Customer, answer.class
   end
 
   def test_customer_repo_returns_nil_with_wrong_find_by_id
-    customer = cr.customers
-    assert_equal nil, customer.find_by_id(679879968)
+    assert_equal nil, cr.find_by_id(679879968)
   end
 
   def test_customer_repo_returns_all_matches_with_first_name
-    customer = cr.customers
-    answer = customer.find_all_by_first_name('Marina')
+    answer = cr.find_all_by_first_name('Marina')
     assert_equal 'Marina', answer[0].first_name
     assert_equal 'Corona', answer[0].last_name
   end
 
   def test_customer_repo_returns_empty_array_with_wrong_first_name
-    customer = cr.customers
-    answer = customer.find_all_by_first_name('lollipops')
+    answer = cr.find_all_by_first_name('lollipops')
     assert_equal [], answer
   end
 
   def test_it_returns_all_matching_last_name_customers
-    customer = cr.customers
-    customer_names = customer.find_all_by_last_name('Williams')
+    customer_names = cr.find_all_by_last_name('Williams')
     assert_equal 1, customer_names.count
     assert customer_names.all? {|name| name.last_name == 'Williams'}
   end
 
   def test_it_returns_empty_array_for_wrong_last_name
-    customer = cr.customers
-    all_names = customer.find_all_by_last_name('watermelon')
+    all_names = cr.find_all_by_last_name('watermelon')
     assert_equal [], all_names
   end
 
