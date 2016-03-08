4f087076cb17850df35c3d9a47deed3870a38cc9
marinacor1 <marinaspam1234@gmail.com>
2016-03-08 09:45:29 -0700

    refactor for tot rev complete

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -242,29 +242,43 @@ class SalesAnalyst
     #returns array with merchants that only sell one item by the month they registered
     #use merchant.created_at
   end
-
-  def revenue_by_merchant(query_id)
-    invoice_ids = []
-    @invoices.each do |invoice|
-      if invoice.merchant_id == query_id
-        invoice_ids << invoice.id
+  def find_invoices_by_merchant_id(query_id)
+      invoice_ids = []
+      @invoices.each do |invoice|
+        if invoice.merchant_id == query_id
+          invoice_ids << invoice.id
+        end
       end
-    end
+      invoice_ids
+  end
+
+  def find_all_successful_sales(invoice_ids)
     good_sales = []
     @transactions.each do |sale|
       if invoice_ids.include?(sale.invoice_id) && sale.result == 'success'
         good_sales << sale.invoice_id
       end
     end
-     total_sales = []
-     good_sales.each do |sale|
-     @invoice_items.each do |item|
-       if item.invoice_id == sale
-         total_sales << (item.quantity * item.unit_price)
-       end
-     end
-     end
-   totals = total_sales.inject(:+)
+    good_sales
+  end
+
+  def find_total_for_good_sales(good_sales)
+    total_sales = []
+    good_sales.each do |sale|
+    @invoice_items.each do |item|
+      if item.invoice_id == sale
+        total_sales << (item.quantity * item.unit_price)
+      end
+    end
+    end
+    total_sales
+  end
+
+  def revenue_by_merchant(query_id)
+    invoice_ids = find_invoices_by_merchant_id(query_id)
+    good_sales = find_all_successful_sales(invoice_ids)
+    total_sales = find_total_for_good_sales(good_sales)
+    totals = total_sales.inject(:+)
   end
 
   def most_sold_item_for_merchant(merchant_id)
