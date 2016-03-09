474f55ef7f2ebde7a2f5e5cdb161653d2c34e0f9
Kami Boers <kamiboers@gmail.com>
2016-03-09 10:21:24 -0700

    invoice alteration

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -2,7 +2,7 @@ require 'pry'
 require 'time'
 
 class Invoice
-attr_accessor :id, :customer_id, :merchant_id, :status, :created_at, :updated_at, :merchant, :customer, :items, :transactions, :total, :total_revenue, :invoice_items
+attr_accessor :id, :customer_id, :merchant_id, :status, :created_at, :updated_at, :merchant, :customer, :items, :transactions, :total, :invoice_items, :paid
   def initialize(attributes)
     @id = attributes[:id].to_i
     @customer_id = attributes[:customer_id].to_i
@@ -13,20 +13,18 @@ attr_accessor :id, :customer_id, :merchant_id, :status, :created_at, :updated_at
     @items = []
     @transactions = []
     @invoice_items = []
+    @paid = nil
   end
 
   def is_paid_in_full?
-    array = all_transaction_status
-    if array.include?("failed") || array.empty?
-      return false
-    else
-      return true
-    end
+    @paid
   end
 
   def is_pending?
-    transactions.all? do |transaction|
-      transaction.result == "failed"
+    if @status == "pending"
+      return true
+    else
+      return false
     end
   end
 
@@ -49,6 +47,8 @@ attr_accessor :id, :customer_id, :merchant_id, :status, :created_at, :updated_at
     Time.parse(@updated_at)
   end
 
-
+  def inspect
+    "#<#{self.class}>"
+  end
 
 end
