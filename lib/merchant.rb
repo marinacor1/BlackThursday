f3f71b6d43b52ef5d7dc2cc9f986f0ed0956a0b9
marinacor1 <marinaspam1234@gmail.com>
2016-03-08 21:25:40 -0700

    adding revenue method for merchant

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -2,7 +2,7 @@ require 'pry'
 require 'time'
 
 class Merchant
-  attr_accessor :id, :name, :created_at, :updated_at, :items, :item_count, :avg_item_price, :invoices, :invoice_count, :customers
+  attr_accessor :id, :name, :created_at, :updated_at, :items, :item_count, :avg_item_price, :invoices, :invoice_count, :customers, :revenue
 
   def initialize(attributes)
     @id = attributes[:id].to_i
@@ -14,6 +14,12 @@ class Merchant
     @customers=[]
   end
 
-  
+  def total_revenue
+    invoice_items.map do |item|
+      item.unit_price * item.quantity
+    end
+  end
+
+
 
 end
