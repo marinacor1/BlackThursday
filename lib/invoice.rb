a7cf515eb2abcb7d2a91f445c92f7afa2b185cf6
Kami Boers <kamiboers@gmail.com>
2016-03-08 18:25:21 -0700

    resolved error in invoice

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -3,6 +3,8 @@ require 'time'
 
 class Invoice
 attr_accessor :id, :customer_id, :merchant_id, :status, :created_at, :updated_at, :merchant, :customer, :items, :transactions, :total
+attr_reader :paid
+
   def initialize(attributes)
     @id = attributes[:id].to_i
     @customer_id = attributes[:customer_id].to_i
@@ -12,12 +14,14 @@ attr_accessor :id, :customer_id, :merchant_id, :status, :created_at, :updated_at
     @updated_at = attributes[:updated_at]
     @items = []
     @transactions = []
+    @paid = true
   end
 
   def is_paid_in_full?
     array = all_transaction_status
     if array.include?("failed") || array.empty?
       return false
+      @paid = false
     else
       return true
     end
