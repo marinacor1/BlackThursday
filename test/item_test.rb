96697f3f96c84bd330cdbdef5a5b3f348691320b
marinacor1 <marinaspam1234@gmail.com>
2016-03-10 08:58:56 -0700

    cleaning indents in test

--- test/item_test.rb
+++ test/item_test.rb
@@ -14,23 +14,23 @@ end
 
 def test_it_instantiates_an_item_object_with_parameters_from_spec
     data = {
-      :name        => "Pencil",
-      :description => "You can use it to write things",
-      :unit_price  => BigDecimal.new(10.99,4),
-      :created_at  => Time.now,
-      :updated_at  => Time.now}
+            :name        => "Pencil",
+            :description => "You can use it to write things",
+            :unit_price  => BigDecimal.new(10.99,4),
+            :created_at  => Time.now,
+            :updated_at  => Time.now}
     item = Item.new(data)
     assert item.instance_of? Item
   end
 
 def test_item_class_has_access_to_item_info
   data = {
-    :name        => "Pencil",
-    :id          => 1232,
-    :description => "You can use it to write things",
-    :unit_price  => BigDecimal.new(10.99,4),
-    :created_at  => Time.now,
-    :updated_at  => Time.now}
+          :name        => "Pencil",
+          :id          => 1232,
+          :description => "You can use it to write things",
+          :unit_price  => BigDecimal.new(10.99,4),
+          :created_at  => Time.now,
+          :updated_at  => Time.now}
   item = Item.new(data)
   assert_equal "Pencil", item.name
   assert_equal "You can use it to write things", item.description
